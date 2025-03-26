cdde1dc579517f7664429e8d56c9c1ec3ed6743b
Lassmara <lassmara1906@gmail.com>
2025-03-27 05:02:01 +0530

    Set base for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Important for relative paths in production
   plugins: [react()],
 })
